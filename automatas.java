3d75f0ddb1bdc81737e4a02ca9a8b4df72e5b6b9
Alfredo <21030761@itcelaya.edu.mx>
2024-03-21 14:01:02 -0600

    Method to encapsulate logic of validate String and file txt with emails added

--- automatas.java
+++ automatas.java
@@ -5,74 +5,77 @@ import java.io.IOException;
 public class automatas {
     public static void main(String[] args) {
         String fileName = "matriz-automata-mail.txt";
-        char[][] matrix = populateMatrix(fileName);
+        char[][] matrizEstados = populatematrizEstados(fileName);
+        String emailsFile = "$$  21030761@itcelaya.edu.mx ju$an.2003@work2 juan.2003@work1 ";   
+        
+        String correosValidos = automataEmails(emailsFile, matrizEstados);     
+        System.out.println("CORREOS VÁLIDOS");
+        System.out.println(correosValidos);
 
-        // Lo que tendría que hacer es leer todo el txt y escribirlo en un String
-        //String textFile = "$$  21030761@itcelaya.edu.mx ju$an.2003@work2 ($.) juan.2003@work1";
-        String textFile = "$$  21030761@itcelaya.edu.mx ju$an.2003@work2 juan.2003@work1 ";
+        
+    }
+    
+    //public static void automataEmails(File emailsFile){
+    public static String automataEmails(String emailsFile, char[][] matrizEstados){
         String correoValido = "";
-
-        // >>>>>>>>>> Parte del autómata <<<<<<<<<<<
+        String correosValidos = "";
+    
         int indexChar = 1;
         int indexEstado = 1;
         
-        for (int indexTextFile = 0; indexTextFile < textFile.length(); indexTextFile++) {
-            while (matrix[0][indexChar] != textFile.charAt(indexTextFile)) {
+        for (int indexemailsFile = 0; indexemailsFile < emailsFile.length(); indexemailsFile++) {
+            while (matrizEstados[0][indexChar] != emailsFile.charAt(indexemailsFile)) {
                 indexChar++;
             }
-
+    
             // EQUIVALENTES EN ASCII del valor 0(48) y 9(57)
-            if(matrix[indexEstado][indexChar] != 48 && matrix[indexEstado][indexChar] != 57){
-                correoValido += textFile.charAt(indexTextFile);
+            if(matrizEstados[indexEstado][indexChar] != 48 && matrizEstados[indexEstado][indexChar] != 57){
+                correoValido += emailsFile.charAt(indexemailsFile);
             }else{
                 correoValido = "";
             }
             
             // El 48 es para que me de el vaor literal, y no ASCII
-            indexEstado = (matrix[indexEstado][indexChar]-48)+1; // El índice de los estados empieza una posición después del inicio
+            indexEstado = (matrizEstados[indexEstado][indexChar]-48)+1; // El índice de los estados empieza una posición después del inicio
             // !!!DEBERÍAN SER 8 Y 9!!!!
             if(indexEstado == 9){
-                // Debe escribir el "correoValido" en el archivo
                 System.out.println("Escribo:"+correoValido);
+                correosValidos += correoValido+"\n";
                 correoValido = "";
                 indexEstado = 1;
             }
             if(indexEstado == 10){
-                while (textFile.charAt(indexTextFile) != ' ' || indexTextFile > textFile.length()) {
-                    indexTextFile++;
+                while (emailsFile.charAt(indexemailsFile) != ' ' || indexemailsFile > emailsFile.length()) {
+                    indexemailsFile++;
                 }
                 indexEstado = 1;
             }
             indexChar = 1;
         }
-
-    }
-
-    //public static void automataEmails(File emailsFile){
-    public static void automataEmails(String emailsFile, char[][] matrizEstados){
         
+        return correosValidos;
     }
 
-    public static char[][] populateMatrix(String fileName){
+    public static char[][] populatematrizEstados(String fileName){
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
 
             int numRows = 9; 
             int numCols = 44; 
 
-            char[][] matrix = new char[numRows][numCols];
+            char[][] matrizEstados = new char[numRows][numCols];
 
             String line;
             int row = 0;
             while ((line = reader.readLine()) != null && row < numRows) {
                 for (int column = 0; column < numCols && column < line.length(); column++) {
-                    matrix[row][column] = line.charAt(column);
+                    matrizEstados[row][column] = line.charAt(column);
                 }
                 row++;
             }
             reader.close();
 
-            return matrix;
+            return matrizEstados;
             
         } catch (IOException e) {
             throw new Error(e);
