3b704d5386f1eb974a38c05b2ad2fbae00bfef03
Alfredo <21030761@itcelaya.edu.mx>
2024-03-21 13:54:33 -0600

    Case 1 working. Porblems with blank spaces at the end of file

--- automatas.java
+++ automatas.java
@@ -1,77 +1,3 @@
-// import java.io.BufferedReader;
-// import java.io.File;
-// import java.io.FileInputStream;
-// import java.io.IOException;
-// import java.io.InputStream;
-// import java.io.InputStreamReader;
-// import java.io.Reader;
-// import java.nio.charset.Charset;
-
-// public class automatas {
-//     public static void main(String[] args) throws IOException {
-//         File file = new File("./matriz-automata-mail.txt");
-//         // Retorna el charset del S.O. 
-//         Charset encoding = Charset.defaultCharset();
-//         readFile(file, encoding);
-        
-//     }
-
-//     private static void readFile(File file, Charset encoding) throws IOException {
-//         try (InputStream in = new FileInputStream(file);
-//              Reader reader = new InputStreamReader(in, encoding);
-//              // buffer para la eficiencia
-//              Reader buffer = new BufferedReader(reader)) {
-//             readCharacters(buffer);
-//         }
-//     }
-
-//     // private static void readCharacters(Reader reader) throws IOException {
-//     //     int r;
-       
-//     //     while ((r = reader.read()) != -1) {
-//     //         // Detectar que es un salto de línea, 
-//     //             // 
-//     //         char ch = (char) r;
-//     //         System.out.println("Do something with " + ch);
-//     //     }
-//     // }
-
-//     private static void readCharacters(Reader reader) throws IOException {
-//         int r;
-//         //int lenghtX = getMatrixWidth(reader);
-//         //System.out.println("X lenght: "+lenghtX);
-
-//         while ((r = reader.read()) != -1) {
-//             char ch = (char) r;
-//             System.out.print(ch+"-");
-//         }
-//     }
-    
-//     // Este método vaciá la promera fila de los caracteres en el archivo
-//     private static int getMatrixWidth(Reader reader) throws IOException {
-//         int r;
-//         int cicle = 1;
-//         int columnLenght = 0;
-//         while ((r = reader.read()) != '\n' && cicle <= 2) {
-//             cicle++;
-//             if (cicle == 2) {
-//                 columnLenght++;
-//             }
-//         }
-//         return columnLenght-1;
-//     }
-
-
-//     private static char[][] getMatrix(File file){
-
-        
-//         short lenghtX = 0, lenghtY = 0;
-//         char[][] matrix = new char[lenghtX][lenghtY];
-
-//         return matrix;
-//     }
-// }
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -82,45 +8,41 @@ public class automatas {
         char[][] matrix = populateMatrix(fileName);
 
         // Lo que tendría que hacer es leer todo el txt y escribirlo en un String
-        String textFile = "$$  21030761@itcelaya.edu.mx ju$an.2003@work1 ($.) juan.2003@work1";
+        //String textFile = "$$  21030761@itcelaya.edu.mx ju$an.2003@work2 ($.) juan.2003@work1";
+        String textFile = "$$  21030761@itcelaya.edu.mx ju$an.2003@work2 juan.2003@work1 ";
         String correoValido = "";
 
         // >>>>>>>>>> Parte del autómata <<<<<<<<<<<
         int indexChar = 1;
         int indexEstado = 1;
-        boolean valido = false; // A lo mejor no es necesario esta bandera 
         
-
         for (int indexTextFile = 0; indexTextFile < textFile.length(); indexTextFile++) {
             while (matrix[0][indexChar] != textFile.charAt(indexTextFile)) {
                 indexChar++;
             }
-            // El correo es válido si el estado no es 9 o 0 
-            // ESTOY ESCRIBIENDO LOS EQUIVALENTES EN ASCII
+
+            // EQUIVALENTES EN ASCII del valor 0(48) y 9(57)
             if(matrix[indexEstado][indexChar] != 48 && matrix[indexEstado][indexChar] != 57){
                 correoValido += textFile.charAt(indexTextFile);
-                valido = true; 
-            }
-            // El correo no es válido
-            else{
+            }else{
                 correoValido = "";
-                valido = false;
             }
             
             // El 48 es para que me de el vaor literal, y no ASCII
-            indexEstado = (matrix[indexEstado][indexChar]-48)+1; // Es más uno porque los estados empiezan una fila después
+            indexEstado = (matrix[indexEstado][indexChar]-48)+1; // El índice de los estados empieza una posición después del inicio
             // !!!DEBERÍAN SER 8 Y 9!!!!
             if(indexEstado == 9){
                 // Debe escribir el "correoValido" en el archivo
                 System.out.println("Escribo:"+correoValido);
+                correoValido = "";
                 indexEstado = 1;
             }
             if(indexEstado == 10){
-                indexEstado = 1; // Aquí es uno porque el estado 0 está en ese índice
+                while (textFile.charAt(indexTextFile) != ' ' || indexTextFile > textFile.length()) {
+                    indexTextFile++;
+                }
+                indexEstado = 1;
             }
-            // System.out.println("File char: "+matrix[indexEstado][indexChar]);
-            // System.out.println("Index estado: "+indexEstado);
-
             indexChar = 1;
         }
 
