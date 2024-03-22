170849c4baa3610f7d99a249c7dedb3604b7ea50
Alfredo <21030761@itcelaya.edu.mx>
2024-03-21 23:09:40 -0600

    Method to process the file.txt and convert it to String

--- automatas.java
+++ automatas.java
@@ -1,21 +1,31 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class automatas {
-    public static void main(String[] args) {
-        String fileName = "matriz-automata-mail.txt";
-        char[][] matrizEstados = populatematrizEstados(fileName);
-        String emailsFile = "$$  21030761@itcelaya.edu.mx ju$an.2003@work2 juan.2003@work1 ";   
-        
-        String correosValidos = automataEmails(emailsFile, matrizEstados);     
-        System.out.println("CORREOS VÁLIDOS");
-        System.out.println(correosValidos);
+    public static void main(String[] args) throws FileNotFoundException {
+        //#1 Convertir el archivo.txt a la matriz de estados
+        String estadosFile = "matriz-automata-mail.txt";
+        char[][] matrizEstados = populatematrizEstados(estadosFile);
 
+        //#2 Procesar el archivo de prueba que contiene los emails
+        // String emailsFile = "automata01.in.txt";
+        // String stringifiedFile = fileToString(emailsFile);
+
+        String stringifiedFile = "   en la ciudad de celaya guanajuato. se considera que existe personal adscrito a dicho municipio en cual cuenta con diferentes correos electronino tales como juan..martinez@com.mx a1a1@@mexico.mx y algunos otros que se han considerado como indefinidos dentros de los diferentes ecosistemas validos para los correos electronicos. el problema radica en que pocas veces pocdemos encotrar los nombres que pertenecen al nombre de l iudad celaya@gto.mx dentro del contexto valido de las personas. por otra parte en la escuela de nombre universidad@celaya.gto.mx. podemos ver como se han asignado diversos correos electronicos a sus estudiantes alguno al parecer son bastan raros o poco comunes para otras persona tales como 1.2.3.4.5@a.b.c y otros muy  adecuandos para la gran mayoria de los ciudadan@s todo esto sin importar cuanto han estado trabajando para identificar los nombres correctos qutene pertenecen a esta gran familia de correos@validos@mx";
         
+        //#3 Obtener el String con los emails válidos
+        String correosValidos = automataEmails(stringifiedFile, matrizEstados);    
+        System.out.println(correosValidos); 
+
+        //#4 Escribir los correos válidos en un archivo 
+        PrintWriter writer = new PrintWriter("automata01.out");
+        writer.println(correosValidos);
+        writer.close();
     }
     
-    //public static void automataEmails(File emailsFile){
     public static String automataEmails(String emailsFile, char[][] matrizEstados){
         String correoValido = "";
         String correosValidos = "";
@@ -27,35 +37,36 @@ public class automatas {
             while (matrizEstados[0][indexChar] != emailsFile.charAt(indexemailsFile)) {
                 indexChar++;
             }
+            // System.out.print(" -"+matrizEstados[0][indexChar]+"- ");
     
-            // EQUIVALENTES EN ASCII del valor 0(48) y 9(57)
             if(matrizEstados[indexEstado][indexChar] != 48 && matrizEstados[indexEstado][indexChar] != 57){
                 correoValido += emailsFile.charAt(indexemailsFile);
             }else{
                 correoValido = "";
             }
             
-            // El 48 es para que me de el vaor literal, y no ASCII
             indexEstado = (matrizEstados[indexEstado][indexChar]-48)+1; // El índice de los estados empieza una posición después del inicio
-            // !!!DEBERÍAN SER 8 Y 9!!!!
+
             if(indexEstado == 9){
-                System.out.println("Escribo:"+correoValido);
+                //System.out.println("Escribo:"+correoValido);
                 correosValidos += correoValido+"\n";
                 correoValido = "";
                 indexEstado = 1;
             }
+
             if(indexEstado == 10){
-                while (emailsFile.charAt(indexemailsFile) != ' ' || indexemailsFile > emailsFile.length()) {
+                while (indexemailsFile < emailsFile.length() && emailsFile.charAt(indexemailsFile) != ' ') {
                     indexemailsFile++;
                 }
                 indexEstado = 1;
             }
             indexChar = 1;
         }
-        
+        if(correosValidos.length() == 0) return "0";
         return correosValidos;
     }
 
+    // TODO: Debe calcular el tamaño de la matríz dinámicamente
     public static char[][] populatematrizEstados(String fileName){
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
@@ -81,5 +92,21 @@ public class automatas {
             throw new Error(e);
             //e.printStackTrace();
         }
+    }
+
+    public static String fileToString(String fileName){
+        StringBuilder fileStringied = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                fileStringied.append(linea).append("\n");
+            }
+            reader.close();
+        } catch (IOException e) {
+            throw new Error(e);
+            //e.printStackTrace();
+        }
+        return fileStringied.toString();
     }
 }
